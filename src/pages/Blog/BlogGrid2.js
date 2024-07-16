702f10b1805241b0699c29dac3193e538c7dbd0b
Abhay Wankhade <abhaywankhade2004@gmail.com>
2024-07-16 21:58:37 +0530

    ggghvg

--- src/pages/Blog/BlogGrid2.js
+++ src/pages/Blog/BlogGrid2.js
@@ -57,7 +57,7 @@ const BlogGrid2 = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://your-wordpress-site.com/wp-json/wp/v2/posts?per_page=3')
+    fetch('https://artizz.in/wp-json/wp/v2/posts?per_page=3')
       .then(response => response.json())
       .then(data => setPosts(data))
       .catch(error => console.error('Error fetching posts:', error));
