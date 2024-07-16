cab1d1664cb228e879be0297101483fc5bfa4552
Abhay Wankhade <abhaywankhade2004@gmail.com>
2024-07-16 19:04:44 +0530

    yes

--- src/pages/Blog/BlogHero.js
+++ src/pages/Blog/BlogHero.js
@@ -3,7 +3,7 @@ import React, { useState , useRef, useEffect} from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+import'./BlogHero.css'
 import { useInView } from 'react-intersection-observer';
 const cards = [
     {
